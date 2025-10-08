62863199cca70566b3ae261cc2dd4e206434a96f
Nick <ngnick.smit@gmail.com>
2025-10-08 13:10:13 +0200

    Post-model Voetballers toegevoegd

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Post
+from .models import Voetballers
 
 admin.site.register(Post)
+admin.site.register(Voetballers)
 
 # Register your models here.
