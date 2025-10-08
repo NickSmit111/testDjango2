4b6652958f1f27a9e1ec99c4bd73f60ebb93ad63
Nick <ngnick.smit@gmail.com>
2025-10-08 13:23:50 +0200

    Post-model Voetballers verwijderd

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from .models import Post
-from .models import Voetballers
 
 admin.site.register(Post)
-admin.site.register(Voetballers)
 
 # Register your models here.
